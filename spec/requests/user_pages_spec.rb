e52cdfe041f51f73fdbebffadc734177fe91859a
lanxi <lhuang38@wisc.edu>
2014-03-11 11:30:15 -0500

    Finish exercises 7.6.1 to 7.6.4

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -24,12 +24,6 @@ describe "signup" do
 
     let(:submit) { "Create my account" }
 
-    describe "with invalid information" do
-      it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
-      end
-    end
-
     describe "with valid information" do
       before do
         fill_in "Name",         with: "Example User"
@@ -41,6 +35,152 @@ describe "signup" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
+
+    describe "with invalid information" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+ 
+     describe "after submission" do
+        before { click_button submit }
+
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+
+    describe "with blank name" do
+        before do
+          fill_in "Name",         with: ""
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+
+    describe "with blank email" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: ""
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+
+    describe "with blank password digest" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: ""
+          fill_in "Confirmation", with: "foobar"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end 
+
+     describe "with blank password confirmation" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: ""
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+
+    describe "with invalid email" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: "foo@bar"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+
+    describe "with password that is too short" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: "foo"
+          fill_in "Confirmation", with: "foo"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+
+      describe "with mismatched password and password confirmation" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: "foobat"
+          fill_in "Confirmation", with: "foobar"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+
+      describe "with email already taken" do
+        before do
+          fill_in "Name",         with: "Foo Bar"
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+          visit signup_path
+          fill_in "Name",         with: "Foo Bar2"
+          fill_in "Email",        with: "foo@bar.com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+        end
+        it "should not create a user" do
+          expect { click_button submit }.not_to change(User, :count)
+        end
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+     end
+    end  
   end
 end
